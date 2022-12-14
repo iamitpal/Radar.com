a65b5daa8149bb121b0b16f62ac8067873970264
iamitpal <amitpalinfo@yahoo.in>
2022-12-14 13:19:24 +0530

    Images

---
+++ radar_project/src/Components/AllRoutes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom ";
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
