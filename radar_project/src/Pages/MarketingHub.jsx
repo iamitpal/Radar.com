0d4e24fb74d43eab617a187ed21214be3941534d
iamitpal <amitpalinfo@yahoo.in>
2022-12-17 18:20:13 +0530

    dev

--- radar_project/src/Pages/MarketingHub.jsx
+++ radar_project/src/Pages/MarketingHub.jsx
@@ -1,7 +1,469 @@
-import React from "react";
+import {
+  Container,
+  SimpleGrid,
+  Image,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  StackDivider,
+  Grid,
+  Icon,
+  useColorModeValue,
+  Avatar,
+  keyframes,
+  Box,
+  Button,
+  IconButton,
+  createIcon,
+  IconProps,
+} from "@chakra-ui/react";
+import {
+  IoAnalyticsSharp,
+  IoLogoBitcoin,
+  IoSearchSharp,
+} from "react-icons/io5";
+import { ReactElement } from "react";
 
-const MarketingHub = () => {
-  return <div>MarketingHub</div>;
+interface FeatureProps {
+  text: string;
+  iconBg: string;
+  icon?: ReactElement;
+}
+
+const Feature = ({ text, icon, iconBg }) => {
+  return (
+    <Stack direction={"row"} align={"center"}>
+      <Flex
+        w={8}
+        h={8}
+        align={"center"}
+        justify={"center"}
+        rounded={"full"}
+        bg={iconBg}
+      >
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{text}</Text>
+    </Stack>
+  );
 };
 
-export default MarketingHub;
+export default function MarketingHub() {
+  const size = "96px";
+  const color = "teal";
+
+  const pulseRing = keyframes`
+	0% {
+    transform: scale(0.33);
+  }
+  40%,
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
+  }
+	`;
+
+  return (
+    <Container maxW={"6xl"} py={12}>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+        <Stack spacing={4}>
+          <Text
+            textTransform={"uppercase"}
+            color={"blue.400"}
+            fontWeight={600}
+            fontSize={"sm"}
+            bg={useColorModeValue("blue.50", "blue.900")}
+            p={2}
+            alignSelf={"flex-start"}
+            rounded={"md"}
+          >
+            Marketing Software
+          </Text>
+          <Heading>Where All Your Marketing Comes Together</Heading>
+          <Text color={"gray.500"} fontSize={"lg"}>
+            With Marketing Hub, all your marketing tools and data are on one
+            easy-to-use, powerful platform. You’ll save valuable time and get
+            all the context you need to provide a personalized experience that
+            attracts and converts the right customers at scale.
+          </Text>
+          <Stack
+            spacing={4}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue("gray.100", "gray.700")}
+              />
+            }
+          >
+            <Feature
+              icon={
+                <Icon as={IoAnalyticsSharp} color={"yellow.500"} w={5} h={5} />
+              }
+              iconBg={useColorModeValue("yellow.100", "yellow.900")}
+              text={"Business Planning"}
+            />
+            <Feature
+              icon={<Icon as={IoLogoBitcoin} color={"green.500"} w={5} h={5} />}
+              iconBg={useColorModeValue("green.100", "green.900")}
+              text={"Financial Planning"}
+            />
+            <Feature
+              icon={
+                <Icon as={IoSearchSharp} color={"purple.500"} w={5} h={5} />
+              }
+              iconBg={useColorModeValue("purple.100", "purple.900")}
+              text={"Market Analysis"}
+            />
+          </Stack>
+        </Stack>
+        <Flex>
+          <Image
+            rounded={"md"}
+            alt={"feature image"}
+            src={
+              "https://www.hubspot.com/hubfs/Marketing-Hub-Featured-Image-EN@2x.png"
+            }
+            objectFit={"cover"}
+          />
+        </Flex>
+      </SimpleGrid>
+      <Box>
+        <br></br>
+      </Box>
+      <Box>
+        <br></br>
+      </Box>
+      <Heading
+        lineHeight={1.1}
+        fontWeight={600}
+        fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
+      >
+        <Text
+          as={"span"}
+          position={"relative"}
+          _after={{
+            content: "''",
+            width: "full",
+            height: "30%",
+            position: "absolute",
+            bottom: 1,
+            left: 0,
+            bg: "#ffbc4b",
+            zIndex: -1,
+          }}
+        >
+          TRUSTED BY,
+        </Text>
+        <br />
+        <Text as={"span"} color={"#ffbc4b"}>
+          use everywhere!
+        </Text>
+      </Heading>
+      <Grid display={"flex"} gridColumn={4}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          h="216px"
+          w="full"
+          overflow="hidden"
+        >
+          {/* Ideally, only the box should be used. The <Flex /> is used to style the preview. */}
+          <Box
+            as="div"
+            position="relative"
+            w={size}
+            h={size}
+            _before={{
+              content: "''",
+              position: "relative",
+              display: "block",
+              width: "300%",
+              height: "300%",
+              boxSizing: "border-box",
+              marginLeft: "-50%",
+              marginTop: "-100%",
+              borderRadius: "50%",
+              bgColor: color,
+              animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
+            }}
+          >
+            <Avatar
+              src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/2017/April/35-Famous-Circle-Logos/19_400.png"
+              size="full"
+              position="absolute"
+              top={0}
+            />
+          </Box>
+        </Flex>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          h="216px"
+          w="full"
+          overflow="hidden"
+        >
+          {/* Ideally, only the box should be used. The <Flex /> is used to style the preview. */}
+          <Box
+            as="div"
+            position="relative"
+            w={size}
+            h={size}
+            _before={{
+              content: "''",
+              position: "relative",
+              display: "block",
+              width: "300%",
+              height: "300%",
+              boxSizing: "border-box",
+              marginLeft: "-50%",
+              marginTop: "-100%",
+              borderRadius: "50%",
+              bgColor: color,
+              animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
+            }}
+          >
+            <Avatar
+              src="https://www.hubspot.com/hubfs/WW%20Logo%20for%20HS%20Website%20(2).png"
+              size="full"
+              position="absolute"
+              top={0}
+            />
+          </Box>
+        </Flex>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          h="216px"
+          w="full"
+          overflow="hidden"
+        >
+          {/* Ideally, only the box should be used. The <Flex /> is used to style the preview. */}
+          <Box
+            as="div"
+            position="relative"
+            w={size}
+            h={size}
+            _before={{
+              content: "''",
+              position: "relative",
+              display: "block",
+              width: "300%",
+              height: "300%",
+              boxSizing: "border-box",
+              marginLeft: "-50%",
+              marginTop: "-100%",
+              borderRadius: "50%",
+              bgColor: color,
+              animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
+            }}
+          >
+            <Avatar
+              src="https://bcassetcdn.com/public/blog/wp-content/uploads/2021/11/30150000/pinterest.jpg"
+              size="full"
+              position="absolute"
+              top={0}
+            />
+          </Box>
+        </Flex>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          h="216px"
+          w="full"
+          overflow="hidden"
+        >
+          {/* Ideally, only the box should be used. The <Flex /> is used to style the preview. */}
+          <Box
+            as="div"
+            position="relative"
+            w={size}
+            h={size}
+            _before={{
+              content: "''",
+              position: "relative",
+              display: "block",
+              width: "300%",
+              height: "300%",
+              boxSizing: "border-box",
+              marginLeft: "-50%",
+              marginTop: "-100%",
+              borderRadius: "50%",
+              bgColor: color,
+              animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
+            }}
+          >
+            <Avatar
+              src="https://extendthemes.com/wp-content/uploads/2018/06/xbox-green-min.png"
+              size="full"
+              position="absolute"
+              top={0}
+            />
+          </Box>
+        </Flex>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          h="216px"
+          w="full"
+          overflow="hidden"
+        >
+          {/* Ideally, only the box should be used. The <Flex /> is used to style the preview. */}
+          <Box
+            as="div"
+            position="relative"
+            w={size}
+            h={size}
+            _before={{
+              content: "''",
+              position: "relative",
+              display: "block",
+              width: "300%",
+              height: "300%",
+              boxSizing: "border-box",
+              marginLeft: "-50%",
+              marginTop: "-100%",
+              borderRadius: "50%",
+              bgColor: color,
+              animation: `2.25s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
+            }}
+          >
+            <Avatar
+              src="https://www.webdesignerdepot.com/cdn-origin/uploads/circular_logos/starbucks.jpg"
+              size="full"
+              position="absolute"
+              top={0}
+            />
+          </Box>
+        </Flex>
+      </Grid>
+
+      <Container maxW={"7xl"}>
+        <Stack
+          align={"center"}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: "2xl", sm: "2xl", lg: "3xl" }}
+            >
+              <br />
+              <Text as={"span"} color={"#ffbc4b"}>
+                All your marketing tools and data — all under one roof.
+              </Text>
+            </Heading>
+            <Text color={"gray.500"}>
+              The job is tough for modern marketers. Either you’re juggling
+              various point solutions, or you’re stuck with tools that are
+              powerful but impossible to use. This means scattered customer
+              data, lackluster marketing, and too much time spent fussing with
+              software. But it doesn’t have to be that way.
+            </Text>
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={{ base: "column", sm: "row" }}
+            >
+              <Button
+                rounded={"full"}
+                size={"lg"}
+                fontWeight={"normal"}
+                px={6}
+                colorScheme={"red"}
+                bg={"#ffbc4b"}
+                _hover={{ bg: "#ffbc4b" }}
+              >
+                Get started
+              </Button>
+              <Button
+                rounded={"full"}
+                size={"lg"}
+                fontWeight={"normal"}
+                px={6}
+                leftIcon={<PlayIcon h={4} w={4} color={"gray.300"} />}
+              >
+                How It Works
+              </Button>
+            </Stack>
+          </Stack>
+          <Flex
+            flex={1}
+            justify={"center"}
+            align={"center"}
+            position={"relative"}
+            w={"full"}
+          >
+            <Blob
+              w={"150%"}
+              h={"150%"}
+              position={"absolute"}
+              top={"-20%"}
+              left={0}
+              zIndex={-1}
+              color={useColorModeValue("red.50", "red.400")}
+            />
+            <Box
+              position={"relative"}
+              height={"300px"}
+              rounded={"2xl"}
+              boxShadow={"2xl"}
+              width={"full"}
+              overflow={"hidden"}
+            >
+              <IconButton
+                aria-label={"Play Button"}
+                variant={"ghost"}
+                _hover={{ bg: "transparent" }}
+                icon={<PlayIcon w={12} h={12} />}
+                size={"lg"}
+                color={"white"}
+                position={"absolute"}
+                left={"50%"}
+                top={"50%"}
+                transform={"translateX(-50%) translateY(-50%)"}
+              />
+              <Image
+                alt={"Hero Image"}
+                fit={"cover"}
+                align={"center"}
+                w={"100%"}
+                h={"100%"}
+                src={
+                  "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
+                }
+              />
+            </Box>
+          </Flex>
+        </Stack>
+      </Container>
+    </Container>
+  );
+}
+const PlayIcon = createIcon({
+  displayName: "PlayIcon",
+  viewBox: "0 0 58 58",
+  d: "M28.9999 0.562988C13.3196 0.562988 0.562378 13.3202 0.562378 29.0005C0.562378 44.6808 13.3196 57.438 28.9999 57.438C44.6801 57.438 57.4374 44.6808 57.4374 29.0005C57.4374 13.3202 44.6801 0.562988 28.9999 0.562988ZM39.2223 30.272L23.5749 39.7247C23.3506 39.8591 23.0946 39.9314 22.8332 39.9342C22.5717 39.9369 22.3142 39.8701 22.0871 39.7406C21.86 39.611 21.6715 39.4234 21.5408 39.1969C21.4102 38.9705 21.3421 38.7133 21.3436 38.4519V19.5491C21.3421 19.2877 21.4102 19.0305 21.5408 18.8041C21.6715 18.5776 21.86 18.3899 22.0871 18.2604C22.3142 18.1308 22.5717 18.064 22.8332 18.0668C23.0946 18.0696 23.3506 18.1419 23.5749 18.2763L39.2223 27.729C39.4404 27.8619 39.6207 28.0486 39.7458 28.2713C39.8709 28.494 39.9366 28.7451 39.9366 29.0005C39.9366 29.2559 39.8709 29.507 39.7458 29.7297C39.6207 29.9523 39.4404 30.1391 39.2223 30.272Z",
+});
+export const Blob = (props: IconProps) => {
+  return (
+    <Icon
+      width={"100%"}
+      viewBox="0 0 578 440"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z"
+        fill="currentColor"
+      />
+    </Icon>
+  );
+};
+//MarketingHub
